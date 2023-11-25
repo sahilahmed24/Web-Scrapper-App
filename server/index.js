3d7c4a32309d2c46dc251e631f5be5edd06ee50f
sahil ahmed <sk9780khan@gmail.com>
2023-11-25 23:40:04 +0530

    Web Scrapping App Completed

--- server/index.js
+++ server/index.js
@@ -1,16 +1,16 @@
-const express = require("express"); // Adding Express
-const app = express(); // Initializing Express
+const express = require("express");
+const app = express();
 const puppeteer = require("puppeteer");
 const fs = require("fs");
 const path = require("path");
 const cors = require("cors");
 
-// app.use(
-//   cors({
-//     origin: "*", //for frontEnd connection
-//     credentials: true,
-//   })
-// );
+app.use(
+  cors({
+    origin: "*", // for frontEnd connection
+    credentials: true,
+  })
+);
 
 app.get("/data", function (req, res) {
   puppeteer.launch().then(async function (browser) {
@@ -20,31 +20,52 @@ app.get("/data", function (req, res) {
       "Access-Control-Allow-Headers",
       "Content-Type, Authorization"
     );
+
     try {
-      const browswer = await puppeteer.launch();
-      const page = await browswer.newPage();
+      const page = await browser.newPage();
       await page.goto("https://rategain.com/blog/");
 
-      const BlogDetails = await page.$$eval(".blog-items article", (element) =>
-        element.map((e) => ({
-          BlogTitle: e.querySelector(".content h6 a").innerText,
-          BlogDate: e.querySelector(".content .blog-detail .bd-item span")
-            .innerText,
-          BlogImgUrl: e
-            .querySelector(".img a[data-bg]")
-            .getAttribute("data-bg"),
-          BlogLikeCount: e.querySelector(".content .zilla-likes span")
-            .innerText,
-        }))
-      );
+      // Get the total number of pages
+      // const totalPages = await page.$eval(".pagination a ", (element) =>
+      //   parseInt(element.textContent.trim())
+      // );
+
+      const totalPages = 20;
+
+      const BlogDetails = [];
+
+      for (let currentPage = 1; currentPage <= totalPages; currentPage++) {
+        if (currentPage !== 1) {
+          // Navigate to the next page
+          await page.goto(`https://rategain.com/blog/page/${currentPage}`);
+        }
+
+        const pageBlogDetails = await page.$$eval(
+          ".blog-items article",
+          (elements) =>
+            elements.map((e) => ({
+              BlogTitle: e.querySelector(".content h6 a").innerText,
+              BlogDate: e.querySelector(".content .blog-detail .bd-item span")
+                .innerText,
+              BlogImgUrl: e
+                .querySelector(".img a[data-bg]")
+                .getAttribute("data-bg"),
+              BlogLikeCount: e.querySelector(".content .zilla-likes span")
+                .innerText,
+            }))
+        );
+
+        BlogDetails.push(...pageBlogDetails);
+      }
 
       console.log(BlogDetails);
 
-      await browswer.close();
+      await browser.close();
 
-      const csvData = BlogDetails.map((BlogDetails) => {
-        return `${BlogDetails.BlogTitle}, ${BlogDetails.BlogDate}, ${BlogDetails.BlogImgUrl}, ${BlogDetails.BlogLikeCount}`;
-      }).join("\n");
+      const csvData = BlogDetails.map(
+        (blog) =>
+          `${blog.BlogTitle}, ${blog.BlogDate}, ${blog.BlogImgUrl}, ${blog.BlogLikeCount}`
+      ).join("\n");
 
       const csvFilePath = path.join(__dirname, "BlogDetails.csv");
       fs.writeFileSync(csvFilePath, csvData);
@@ -67,5 +88,5 @@ app.listen(7000, () => {
 });
 
 app.get("/", (req, res) => {
-  res.send(`<h1>this is default Route</h1>`);
+  res.send(`<h1>this is the default Route</h1>`);
 });
